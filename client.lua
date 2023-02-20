f01f31e7c7175eda6992168869a21231bf5f6f81
Chance <79997938+Chaannce@users.noreply.github.com>
2023-02-20 07:45:14 +0000

    Create client.lua

---
+++ client.lua
@@ -0,0 +1,52 @@
+local HttpService = game:GetService("HttpService")
+local RequestFunction = (syn or syn.request) or request or http_request
+local UILibrary = loadstring(game:HttpGet("https://raw.githubusercontent.com/laderite/bleklib/main/library.lua"))()
+local logo = {"ChatGPT in ROBLOX! - ChatRBX", "Created by Cyros and PixelPenguin"}
+
+for _, v in pairs(logo) do
+    print(v)
+end
+
+local Window = UILibrary:Create({
+    Name = "ChatRBX",
+    StartupSound = {
+        Toggle = true,
+        SoundID = "rbxassetid://6958727243",
+        TimePosition = 1
+    }
+})
+
+local MainTab = Window:Tab("Main")
+local OptionsTab = Window:Tab("Options")
+
+-- Main Tab:
+
+MainTab:Label("Make sure you press 'F9' to see messages")
+
+local Message
+
+MainTab:Textbox("Send Text", function(v)
+    Message = v
+end)
+
+MainTab:Button("Send Message", function()
+    print("Client > " .. Message)
+    
+    local Message_Encoded = HttpService:UrlEncode(Message)
+    
+    local response = syn.request({
+        Url = "https://eurythmical-salvage.000webhostapp.com/chatRBXapi.php?prompt=" .. Message_Encoded,
+        Method = "GET"
+    })
+
+    print("Server > " .. response.Body)
+end)
+
+-- Options / UI Tab:
+
+OptionsTab:Button("Destroy UI", function()
+    print("Ending...")
+    task.wait(.5)
+    Window:Exit()
+    print("---------------------------------------")
+end)
